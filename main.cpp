38c90bdd3bd7a9e4f875fb72903bb920803ef880
jazmin9930 <94101171+jazmin9930@users.noreply.github.com>
2021-11-17 21:19:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream> 
+#include <fstream> 
+using namespace std; 
+ 
+//Jazmin 
+int main() { 
+    string lineaActual, primerLinea; 
+    ifstream archivo("JSON.txt"); 
+    int llavesAbiertas = 0; 
+    bool inicioAnalisis = true; 
+ 
+     (getline(archivo, lineaActual));//Esta instruccion es para pasar de una linea del texto, a la siguiente. 
+ 
+    while(lineaActual==""){ //instruccion que ignora las lineas en blanco, itera hasta encontrar la linea con la "{" 
+        getline(archivo, lineaActual); 
+    } 
+ 
+ 
+    
+    return 0; 
+}
